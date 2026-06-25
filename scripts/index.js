aa9d10f8fc65611ab4ddb026cd47fd0744d7eb7d
Sindhu-2017 <sindhujas388@gmail.com>
2026-06-25 08:14:04 +0530

    Landing,index and signup page UI

---
+++ scripts/index.js
@@ -0,0 +1,99 @@
+const API="http://localhost:3000/users";
+
+$("#login").on("click",async function(){
+    let email=$("#email").val().trim();
+    let password=$("#password").val().trim();
+
+    let isValid=true;
+
+    if(email === ""){
+        $("#email").addClass("is-invalid").removeClass("is-valid");
+        isValid=false;
+    }
+    else{
+        $("#email").addClass("is-valid").removeClass("is-invalid");
+    }
+    if(password === ""){
+        $("#password").addClass("is-invalid").removeClass("is-valid");
+        isValid=false;
+    }
+    else{
+        $("#password").addClass("is-valid").removeClass("is-invalid");
+    }
+
+    if(!isValid){
+        await Swal.fire({
+            icon:"warning",
+            title:"Missing Fields",
+            text:"Fill all the fiels"
+        });
+        return;
+    }
+
+    try{
+        const response=await $.ajax({
+            url:API,
+            method:"GET"
+        });
+
+        let validUser=response.find(user => 
+            email === user.Email && password === user.Password
+        );
+
+        if(validUser){
+
+            const result= await Swal.fire({
+                title:"login",
+                text:"Are you sure you want to login?",
+                icon:"question",
+                showCancelButton:true,
+                cancelButtonColor:"#d33",
+                confirmButtonColor:"#3085d6",
+                confirmButtonText:"Yes,login"
+            });
+
+            if(result.isConfirmed){
+                localStorage.setItem("loggedInUser",JSON.stringify(validUser));
+                await Swal.fire({
+                    toast:true,
+                    icon:"success",
+                    title:"redirecting",
+                    position:"top-end",
+                    showCancelButton:false,
+                    timer:2000,
+                    timerProgressBar:true
+                });
+
+                $("#email").val("");
+                $("#password").val("");
+
+                setTimeout(()=>{
+                    if(validUser.Role === "Applicant" ){
+                        window.location.href="pages/applicant_dashboard.html";
+                    }
+                    else if(validUser.Role === "Recruiter"){
+                        window.location.href="pages/recruiter_dashboard.html";
+                    }
+                },2000);
+            }           
+            
+        }
+        if(!validUser){
+            await Swal.fire({
+                icon:"error",
+                title:"Login Failed",
+                text:"Invalid username or password"
+            });
+            return;
+        }
+
+    }
+    catch(error){
+        await Swal.fire({
+            icon:"error",
+            title:"Error",
+            text:"Error occurred"
+        });
+    }
+
+});
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,5 @@
+const API="http://localhost:3000/users";
+
 //fullname validation
 $("#fullname").on("input",function(){
     let namePattern=/^[A-Za-z ]+$/;
@@ -38,20 +40,113 @@ let maxDate=new Date(today.getFullYear()-18 , today.getMonth() , today .getDate(
 $("#dob").attr("max",maxDate.toISOString().split("T")[0]);
 
 
+$("#dob").on("change",function(){
+    let dob=new Date($("#dob").val());
+
+    if(dob > maxDate){
+        $("#dob").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#dob").removeClass("is-invalid").addClass("is-valid");
+    }
+})
 //gender validation
+
 $("input[name='gender']").on("change",function(){
-    $("#genderError").addClass("d-none");
+     $("#genderError").addClass("d-none");
 });
 
 //role validation
 
 $("#role").on("change",function(){
+
+    $("#roleError").addClass("d-none").removeClass("d-block");
   
     if($(this).val() === "Recruiter"){
         $("#companyName").addClass("d-block").removeClass("d-none");
+        $("#companyLocation").addClass("d-block").removeClass("d-none")
+
+        $("#applicantQual").removeClass("d-block").addClass("d-none");
+        $("#applicantExp").removeClass("d-block").addClass("d-none");
+        $("#skillContainer").removeClass("d-block").addClass("d-none");
+
     }
     else if($(this).val() === "Applicant"){
-        $().addClass("d-block");
+        $("#applicantQual").addClass("d-block").removeClass("d-none");
+        $("#applicantExp").addClass("d-block").removeClass("d-none");
+        $("#skillContainer").addClass("d-block").removeClass("d-none");
+
+
+        $("#companyName").removeClass("d-block").addClass("d-none");
+        $("#companyLocation").removeClass("d-block").addClass("d-none")
+    }
+});
+
+
+//company name validation
+$("#compName").on("input",function(){
+    if($("#compName").val().length < 6){
+         $("#compName").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#compName").removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
+//company location validation
+$("#compLocation").on("input",function(){
+    if($("#compLocation").val().length < 6){
+         $("#compLocation").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#compLocation").removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
+//qualification vallidation
+$("#qualification").on("input",function(){
+    if($("#qualification").val().length < 3){
+         $("#qualification").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#qualification").removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
+//experience validation
+$("#experience").on("input",function(){
+    if($("#experience").val()=== ""){
+         $("#experience").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#experience").removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
+
+//skills validation
+$("input[name='skills']").on("change",function(){
+     $("#skillError").addClass("d-none");
+});
+
+//password validation
+$("#password").on("input",function(){
+
+    let passPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$/;
+    if(!passPattern.test($(this).val().trim())){
+         $(this).addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $(this).removeClass("is-invalid").addClass("is-valid");
+    }
+});
+
+$("#confirm").on("input",function(){
+    if($("#confirm").val() === "" || $("#confirm").val() !== $("#password").val()){
+         $("#confirm").addClass("is-invalid").removeClass("is-valid");
+    }
+    else{
+        $("#confirm").removeClass("is-invalid").addClass("is-valid");
     }
 });
 
@@ -80,14 +175,116 @@ $("#signupForm").on("submit",async function(e){
         isValid=false;
     }
    
-    if($(this).val() === ""){
-        $(this).addClass("is-invalid").removeClass("is-valid");
+    if(!$("input[name='gender']:checked").length){
+        $("#genderError").removeClass("d-none");
+        isValid=false;
     }
+
+    if($("#role").val() === ""){
+        $("#roleError").removeClass("d-none").addClass("d-block");
+        isValid=false;
+    }
+
+    if($("#role").val() === "Recruiter"){
+        if($("#compName").val() === ""){
+            $("#compName").addClass("is-invalid").removeClass("is-valid");
+            isValid=false;
+        }
+        
+        if($("#compLocation").val() === ""){
+            $("#compLocation").addClass("is-invalid").removeClass("is-valid");
+            isValid=false;
+
+        }
+
+    }
+    else if($("#role").val() === "Applicant"){
+        if($("#qualification").val() === ""){
+            $("#qualification").addClass("is-invalid").removeClass("is-valid");
+            isValid=false;
+
+        }
+
+        if($("#experience").val() === ""){
+            $("#experience").addClass("is-invalid").removeClass("is-valid");
+            isValid=false;
+        }
+    }
+
     
 
+    if($("input[name='skills']:checked").length < 1){
+        $("#skillError").removeClass("d-none");
+        isValid=false;
+    }
+
+    let passPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$/;
+    if(!passPattern.test($("#password").val().trim())){
+         $("#password").addClass("is-invalid").removeClass("is-valid");
+        isValid=false;
+
+    }
+
+    if($("#confirm").val() === "" || $("#confirm").val() !== $("#password").val()){
+         $("#confirm").addClass("is-invalid").removeClass("is-valid");
+         isValid=false;
+    }
+
+    let dob=new Date($("#dob").val());
+    if($("#dob").val() === "" || dob > maxDate){
+        $("#dob").addClass("is-invalid").removeClass("is-valid");
+         isValid=false;
+    }
 
     if(!isValid){
         return;
+    }
+
+    let users={
+        Fullname:$("#fullname").val().trim().split(" ").map(word=> word.charAt(0).toUpperCase()+word.slice(1).toLowerCase()).join(" "),
+        Email:$("#email").val().trim(),
+        Phone:$("#phone").val().trim(),
+        DateOfBirth:$("#dob").val(),
+        Gender:$("input[name='gender']:checked").val(),
+        Role:$("#role").val(),
+        CompanyName:$("#compName").val().trim(),
+        CompanyLocation:$("#compLocation").val().trim(),
+        Qualification:$("#qualification").val().trim(),
+        Experience:$("#experience").val().trim(),
+        Skills:$("input[name='skills']:checked").map(function(){
+            return $(this).val()
+        }).get(),
+        Password:$("#password").val()
+    }
+
+    try{
+        await $.ajax({
+            url:API,
+            method:"POST",
+            contentType:"application/json",
+            data:JSON.stringify(users)
+        });
+
+        await Swal.fire({
+            icon:"success",
+            title:"User Added",
+            text:"User Added Successfully!"
+        });
+
+        $("#signupForm")[0].reset();
+
+        setTimeout(()=>{
+            window.location.href="../index.html";
+        },2000);      
+
+
+    }
+    catch(error){
+        await Swal.fire({
+            icon:"error",
+            title:"Error Occured",
+            text:"Cannot able to add User"
+        });
     }
 });
 
